939af1d097393b11812b712a39208c9a64b34780
hitbear518 <hitbear518@qq.com>
2013-08-19 18:57:22 +0800

    c

--- Bookkeeper/src/wangsen/bookkeeper/BillListFragment.java
+++ Bookkeeper/src/wangsen/bookkeeper/BillListFragment.java
@@ -2,14 +2,20 @@ package wangsen.bookkeeper;
 
 import wangsen.bookkeeper.provider.BookkeeperContract.BillTable;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ImageButton;
 
 /**
@@ -17,19 +23,42 @@ import android.widget.ImageButton;
  */
 public class BillListFragment extends ListFragment implements
 		LoaderCallbacks<Cursor> {
-
-	// Views
+	
 	private ImageButton mAddBtn;
-
 	private BillCursorAdapter mAdapter;
+	
+	private static final int LOADER_ID = 0; 
+	private static final int DELETE_ID = Menu.FIRST + 1;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		getLoaderManager().initLoader(0, null, this);
+		getLoaderManager().initLoader(LOADER_ID, null, this);
 		mAdapter = new BillCursorAdapter(getActivity(), null, 0);
 		setListAdapter(mAdapter);
+	}
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		menu.add(0, DELETE_ID, 0, R.string.menu_delete);
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case DELETE_ID:
+			AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+			Uri  uri = Uri.parse(BillTable.CONTENT_URI + "/" + info.id);
+			getActivity().getContentResolver().delete(uri, null, null);
+			break;
+
+		default:
+			break;
+		}
+		return super.onContextItemSelected(item);
 	}
 
 	@Override
@@ -52,28 +81,38 @@ public class BillListFragment extends ListFragment implements
 
 		return root;
 	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		registerForContextMenu(getListView());
+	}
 
 	@Override
 	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-		String[] projection = { BillTable._ID,
-				BillTable.COLUMN_NAME_ADULTS_COUNT,
-				BillTable.COLUMN_NAME_CHILDREN_COUNT,
-				BillTable.COLUMN_NAME_PAYMENKT, BillTable.COLUMN_NAME_TIME,
-				BillTable.COLUMN_NAME_PAYMENT_CHECK };
-
-		String sortOrder = BillTable.COLUMN_NAME_TIME + " DESC";
-		CursorLoader cursorLoader = new CursorLoader(getActivity(),
-				BillTable.CONTENT_URI, projection, null, null, sortOrder);
-		return cursorLoader;
+		switch (id) {
+		case LOADER_ID:
+			String[] projection = { BillTable._ID,
+					BillTable.COLUMN_NAME_ADULTS_COUNT,
+					BillTable.COLUMN_NAME_CHILDREN_COUNT,
+					BillTable.COLUMN_NAME_PAYMENKT, BillTable.COLUMN_NAME_TIME,
+					BillTable.COLUMN_NAME_PAYMENT_CHECK };
+			String sortOrder = BillTable.COLUMN_NAME_TIME + " DESC";
+			CursorLoader cursorLoader = new CursorLoader(getActivity(),
+					BillTable.CONTENT_URI, projection, null, null, sortOrder);
+			return cursorLoader;
+		default:
+			return null;
+		}
 	}
 
 	@Override
 	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-		mAdapter.swapCursor(data);
+		mAdapter.changeCursor(data);
 	}
 
 	@Override
 	public void onLoaderReset(Loader<Cursor> loader) {
-		mAdapter.swapCursor(null);
+		mAdapter.changeCursor(null);
 	}
 }
